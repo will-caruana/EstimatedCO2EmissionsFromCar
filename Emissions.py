487cd7bc51174b41dff25d7d4cee33a6aaf22b28
will-caruana <20567604+will-caruana@users.noreply.github.com>
2023-01-18 21:02:11 -0500

    Update Emissions.py
    Updated the script to take atmospheric conditions into account to estimate CO2 emissions.

--- Emissions.py
+++ Emissions.py
@@ -18,6 +18,16 @@ fuel_cmd = obd.commands.FUEL_RATE
 fuel_response = connection.query(fuel_cmd)
 fuel_consumption = fuel_response.value.magnitude
 
+# Get temperature
+temp_cmd = obd.commands.AMBIANT_AIR_TEMP
+temp_response = connection.query(temp_cmd)
+temperature = temp_response.value.magnitude
+
+# Get pressure
+pressure_cmd = obd.commands.BAROMETRIC_PRESSURE
+pressure_response = connection.query(pressure_cmd)
+pressure = pressure_response.value.magnitude
+
 # Estimate CO2 emissions
-emissions = (fuel_consumption * 3600) / (speed / 3.6)
-print("Estimated CO2 emissions: {:.2f} g/km".format(emissions))
+co2_emission = (fuel_consumption * (652 + (1740*(273+temperature)/273*(pressure/1013.25)**0.288)))/(speed/3.6)
+print("Estimated CO2 emissions: {:.2f} g/km".format(co2_emission))
